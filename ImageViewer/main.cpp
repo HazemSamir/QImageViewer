a4b4f57936b468852d17f7912220b1771408f595
Hazem Samir <hazemhazem.1995@gmail.com>
2017-01-29 04:35:14 +0200

    ADD android manifest for icons and opening files

--- ImageViewer/main.cpp
+++ ImageViewer/main.cpp
@@ -1,12 +1,39 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtAndroidExtras/QAndroidJniObject>
+#include <QtAndroidExtras/QtAndroid>
+
+QString loadAndroidFile();
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    #ifdef Q_OS_ANDROID
+    w.display(loadAndroidFile());
+    #else
     if (argc > 1)
         w.display(argv[1]);
+    #endif
     return a.exec();
+}
+
+QString loadAndroidFile()
+{
+    #ifdef Q_OS_ANDROID
+    QAndroidJniObject activity = QtAndroid::androidActivity();
+    if (activity.isValid()) {
+        QAndroidJniObject intent = activity.callObjectMethod("getIntent", "()Landroid/content/Intent;");
+        if (intent.isValid()) {
+            QAndroidJniObject data = intent.callObjectMethod("getData", "()Landroid/net/Uri;");
+            if (data.isValid()) {
+                QAndroidJniObject path = data.callObjectMethod("getPath", "()Ljava/lang/String;");
+                if (path.isValid())
+                    return path.toString();
+            }
+        }
+    }
+    #endif
+    return "";
 }
